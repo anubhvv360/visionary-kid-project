b9552021d707effebd4db71ea97a3bf2a632e5e6
anubhvv360 <anubhav.verma360@gmail.com>
2025-04-25 19:01:37 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,8 +21,8 @@ genai_client = genai.Client(api_key=st.secrets["GOOGLE_API_KEY"])
 uploaded_file = st.file_uploader(
     "1️⃣ Upload a photo of your kid",
     type=["png", "jpg", "jpeg"],
+    st.caption = ("Images uploaded are not saved, the AI model only extracts the features for one active session.")
 )
-st.caption = ("Images uploaded are not saved, the AI model only extracts the features for one active session.")
 name = st.text_input("2️⃣ What’s your kid’s name?", placeholder="e.g. Robert")
 
 builtin = ["Different Professions", "Value-Based Adventures", "Cultural Landmarks"]
